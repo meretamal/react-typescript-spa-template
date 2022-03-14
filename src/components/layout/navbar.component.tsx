7d00c0f2ee08435d3918fbcb578a9c3c22a82b06
meretamal <meretamal@uc.cl>
2022-03-14 20:13:49 -0300

    feat(theme): add dark mode support

--- src/components/layout/navbar.component.tsx
+++ src/components/layout/navbar.component.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, HStack, Text, useColorMode } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
 export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode()
   return (
     <Box p="4" bgColor="teal.300" boxShadow="md">
-      <Text fontSize='lg' fontWeight="bold" color="white">React Typescript SPA Starter</Text>
+      <HStack justify="space-between" align="center">
+        <Text fontSize='lg' fontWeight="bold" color="white">React Typescript SPA Starter</Text>
+        {colorMode === 'light' ? (
+          <MoonIcon color="white" role="button" onClick={toggleColorMode} />
+        ) : (
+          <SunIcon color="white" role="button" onClick={toggleColorMode}/>
+        )}
+      </HStack>
     </Box>
   );
 }
