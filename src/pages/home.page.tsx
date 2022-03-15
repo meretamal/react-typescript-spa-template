ab37f555fca688481f5a39f5ee33bdf09f7a833d
meretamal <meretamal@uc.cl>
2022-03-14 21:24:45 -0300

    fix: fix min height so that it occupies the remaining viewport vertical space

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -3,7 +3,7 @@ import { Center, Heading } from '@chakra-ui/react';
 
 export default function HomePage() {
   return (
-    <Center sx={{ minHeight: 'calc(100vh - 86px)' }}>
+    <Center sx={{ minHeight: 'calc(100vh - 59px)' }}>
       <Heading size="4xl" as="h1" color="teal.300" textAlign="center" mt="4">
         React Typescript SPA Starter
       </Heading>
