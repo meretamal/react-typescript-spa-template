2222b798252e1630845e0f274022cc76a39cbb58
meretamal <meretamal@uc.cl>
2022-03-16 08:54:50 -0300

    sytle: prefer inline default export

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import Router from './router';
 
-function App() {
+export default function App() {
   return (
     <ChakraProvider>
       <Router />
     </ChakraProvider>
   );
 }
-
-export default App;
