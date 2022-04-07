e5e26e3ee267999fe5ff51c5eb60dea32c26ebc0
meretamal <meretamal@uc.cl>
2022-04-07 12:23:06 -0400

    feat(env): manage api url through env variables

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+const config = {
+  apiUrl: import.meta.env.VITE_APP_API_URL,
+};
+
+export default config;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_API_URL: string | undefined;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
