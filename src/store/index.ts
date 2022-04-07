71c2e84f3b281f4758c2ca42ad7cf40b0836dfc3
meretamal <meretamal@uc.cl>
2022-04-07 12:23:32 -0400

    feat(store): add a base rtk query service

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import baseApi from '@/api/base.api';
 import reducer from './modules';
 
 export const store = configureStore({
   reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
